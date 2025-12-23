9aaf87109715795a84ea87395c9982dbbd303b3b
bresoGit <bresanIvanGit@outlook.com>
2025-12-23 21:46:53 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: ["jtt-fe.onrender.com"],
+    host: true, // same as 0.0.0.0
+    port: 4173, // optional; Render will still use $PORT if you pass it in command
+  },
+});
